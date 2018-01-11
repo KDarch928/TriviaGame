2a6eb83fd78d7888fac8286a09e05feb02d2a430
KDarch928 <Kayle.Drucker@gmail.com>
2018-01-10 23:59:49 -0600

    added hide and view on div when button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,7 @@
 console.log("test");
+
+$("#startBtn").on("click",function () {
+    console.log(this);
+    $("#startScreen").addClass("invisible");
+    $("#triviaQuestions").removeClass("invisible");
+});
